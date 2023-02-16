625558d8641c23e8e03890f0b8dbeb3e1ef9ba0f
benracha <pat.benracha@gmail.com>
2023-02-16 15:33:32 +0700

    test

---
+++ src/components/Data.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Data.css";
+
+function Data() {
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th>a</th>
+                    <th>b</th>
+                    <th>c</th>
+                    <th>d</th>
+                    <th>e</th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+export default Data;
